c7ad5dd02bbd3fbe612b615cc4a348c6bba320e5
noahtt7 <noahtaguma@gmail.com>
2025-01-02 11:39:49 -0800

    Added React Routing; Two working routes that show the stay table component.

--- netomari-frontend/src/App.jsx
+++ netomari-frontend/src/App.jsx
@@ -4,18 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import ListStayComponent from './components/ListStayComponent'
 import { HeaderComponent } from './components/HeaderComponent'
 import { FooterComponent } from './components/FooterComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div >
-      <HeaderComponent/>
-      <div className='content'>
-        <h1 className="text-center">Netomari.</h1>
-        <ListStayComponent/>
-      </div>
-      <FooterComponent/>
+      <BrowserRouter>
+        <HeaderComponent/>
+          <div className='content'>
+            <h1 className="text-center">Netomari.</h1>
+            {/* <ListStayComponent/> */}
+            <Routes>
+                {/* // http://localhost:5173 */}
+                <Route path='/' element = { <ListStayComponent/> }/>
+                {/* // http:/localhost:5173/stays */}
+                <Route path='/stays' element = { <ListStayComponent/> }/>
+            </Routes>
+          </div>
+        <FooterComponent/>
+      </BrowserRouter>
     </div>
   )
 }
